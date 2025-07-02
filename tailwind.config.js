315a990f7cab005bafe461ad5924ac1fe58cfda9
Nathan62-bit <nathanmendonca76@gmail.com>
2025-07-02 17:47:42 +0530

    service

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}", // 👈 tells Tailwind where to look
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
